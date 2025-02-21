fa52ad79522f153fcc0ec8eaadc547efa61e1aa1
schultpo-git <schultpo@mail.uc.edu>
2025-02-20 19:27:11 -0500

    removed file name option, checking valid file names would take too long and I realized that the a-zA-Z and so on are regex themself so no need to include any more regex. Now I just need to figure out why only using -s still gives numbers

--- password_generator.sh
+++ password_generator.sh
@@ -3,10 +3,9 @@
 
 # Display script usage/helpful information
 function display_usage() {
-    echo "Usage: $0 -l <length> -f <filename> [-n] [-s]"
+    echo "Usage: $0 -l <length> [-n] [-s]"
     echo "Options:"
     echo "  -l <length>: Length of the password (between 8 and 20)"
-    echo "  -f <filename>: Name of the file the password will be stored in"
     echo "  -n: Include numbers in the password"
     echo "  -s: Include symbols in the password"
     echo "  -h: Display this help page"
@@ -25,10 +24,10 @@ include_numbers=false
 include_symbols=false
 letters="a-zA-Z"
 numbers="0-9"
-symbols="!@#$%^&*()-_=+[]{}|;:,.<>?/\`~"
+symbols="!@#$%^&*()-_=[]{}|;:,.<>?/\`~"
 
 # Process CL options
-while getopts ":l:f: n s h" opt; do
+while getopts ":l: n s h" opt; do
     case $opt in
         l) # Length of password
             length=$OPTARG
@@ -36,10 +35,6 @@ while getopts ":l:f: n s h" opt; do
                 echo "Password length must be between 8 and 20"
                 exit 1
             fi
-        ;;
-        f) #File name
-            filename=$OPTARG
-            echo $filename
         ;;
         n) # Include numbers
             include_numbers=true
@@ -74,13 +69,6 @@ if [[ $length -eq 0 ]]; then
     exit 1
 fi
 
-# If file name was not set, display help page
-if [[ -z $filename || $filename == "-n" || $filename == "-s" ]]; then
-    echo "Error: The name of the file the password will be stored in must be specified. Please see the usage documentation below."
-    display_usage
-    exit 1
-fi
-
 # Characters to be used for password generation
 characters=$letters
 
@@ -91,11 +79,9 @@ if $include_symbols; then
     characters+=$symbols
 fi
 
-echo "$characters"
-
 # Generate the password
 password=$(head /dev/urandom | base64 | tr -dc "$characters" | head -c "$length")
 
 # Store the password
-echo "$password"
-echo "$password" > ./$filename.txt
+echo "$password" > ./super_secret.txt
+echo "Your new password has been stored in super_secret.txt"
